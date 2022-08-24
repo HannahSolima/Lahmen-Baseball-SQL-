b774f73a3542ee850bfb720b986324fc85e7bc7b
Hannah Solima <hannahsolima@gmail.com>
2022-08-23 21:09:01 -0500

    finishing 4

--- scripts/baseball_project.sql
+++ scripts/baseball_project.sql
@@ -13,3 +13,25 @@ WHERE height = 43
 
 --3. Find all players in the database who played at Vanderbilt University. First/Last names, total salary in Maj. league, sort DESC tots. sal 
 --Which Vanderbilt player earned the most money in the majors? 
+SELECT namefirst, namelast, schoolid, SUM(salary) AS total_salary
+FROM people
+JOIN collegeplaying
+USING(playerid)
+JOIN salaries 
+USING(playerid)
+WHERE schoolid = 'vandy'
+GROUP BY namelast, namefirst, schoolid
+ORDER BY total_salary DESC
+--player David Price earned the most. He made $245,553,888 
+
+--4.Fielding Table, group players (3) on position. OF = Outfield; SS,1B, 2B, 3B = Infield; P,C = Battery
+--Number of putouts? 
+
+SELECT SUM(Distinct PO) AS put_outs, 
+CASE WHEN POS = 'OF' THEN 'Outfield'
+  WHEN POS = 'SS' OR POS LIKE '%B' THEN 'Infield'
+  ELSE 'Battery' END AS group_positions
+FROM fielding
+GROUP BY group_positions
+ORDER BY put_outs DESC
+
